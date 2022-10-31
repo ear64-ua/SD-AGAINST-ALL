26b969572d38c4ba0814aa8e7f76d51cf44a76d1
Pedro J. Belmonte Vera <pedro.j.belmonte.vera@gmail.com>
2022-10-31 09:11:09 +0100

    Saco la clase Modulo de AA_Player a un fichero de clases

---
+++ src/classes.py
@@ -0,0 +1,26 @@
+import json
+
+class Modulo:
+    
+    def __init__(self,id):
+
+        file = open('src/json_files/addresses.json')
+        data = json.load(file)
+        file.close()
+
+        for dir in data['direcciones']:
+            if dir['Id'] == id:
+                self.ip = dir['IP']
+                self.port = int(dir['port'])
+
+    def setIp(self,ip):
+        self.ip = ip
+    
+    def setPort(self,port):
+        self.port = port
+    
+    def getIp(self):
+        return self.ip
+    
+    def getPort(self):
+        return self.port
